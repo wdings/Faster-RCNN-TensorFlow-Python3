7f00130d0d7681f7084982fac6543791d66d39aa
wdings <35138113+wdings@users.noreply.github.com>
2019-10-13 10:55:07 +0800

    Add files via upload

---
+++ sotabench.py
@@ -0,0 +1,12 @@
+from sotabencheval.image_classification import ImageNetEvaluator
+
+evaluator = ImageNetEvaluator(
+    # automatically compare to this paper
+    model_name='Faster-RCNN-TensorFlow-Python3-master',
+    paper_arxiv_id='1506.01497'
+)
+
+predictions = ... # use your model to make predictions
+
+evaluator.add(predictions)
+evaluator.save()
